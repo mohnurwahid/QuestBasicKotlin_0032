5010ee636adce17ef4b302884ce3097186ddf3ee
mohnurwahid <mohnurwahid923@gmail.com>
2024-10-12 02:22:33 +0700

    fun main

--- app/src/main/java/com/example/kotlin/Functions.kt
+++ app/src/main/java/com/example/kotlin/Functions.kt
@@ -17,3 +17,27 @@ fun withNamedArgument(name: String, age: Int){
     println("== withNamedArgument ==")
     println("Hello, $name! You are $age years old.")
 }
+
+fun withDefaultParameter(name: String = "Jamal", age: Int){
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang: Int, lebar: Int): Int {
+    println()
+    println("== withReturn ==")
+    val result = panjang * lebar
+    println(result)
+    return result
+
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "john", age = 30)
+    withDefaultParameter(age = 25)
+    withReturn(5,10)
+
+}
