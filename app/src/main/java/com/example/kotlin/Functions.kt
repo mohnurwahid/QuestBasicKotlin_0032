53861b868863deaa4c52ecd871fad9e6e6ac8f26
mohnurwahid <mohnurwahid923@gmail.com>
2024-10-12 02:20:19 +0700

    membuat fun withParameter

--- app/src/main/java/com/example/kotlin/Functions.kt
+++ app/src/main/java/com/example/kotlin/Functions.kt
@@ -5,3 +5,9 @@ fun withoutParameter(){
     println("== withoutParameter ==")
     println("Hello, World")
 }
+
+fun withParameter(name: String){
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
