127fe2a863499c401f5ee7e67c81ebeb3501cf29
mohnurwahid <mohnurwahid923@gmail.com>
2024-10-12 02:01:21 +0700

    membuat val shape

--- app/src/main/java/com/example/kotlin/Collecctions.kt
+++ app/src/main/java/com/example/kotlin/Collecctions.kt
@@ -6,4 +6,21 @@ fun ContohList() {
 
     val readOnlyAbjad = listOf("A", "B", "C")
     println(readOnlyAbjad)
+
+val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+println(shape)
+
+shape.add("Circle")
+println(shape)
+
+shape.remove("Triangle")
+println(shape)
+
+shape[0] = "Oval"
+println(shape
+)
+
+val shapesLocked: List<String> = shape
+println(shapesLocked)
+
 }
