53cc7079158ece22f987213514d64de868620337
mohnurwahid <mohnurwahid923@gmail.com>
2024-10-12 02:21:16 +0700

    membuat fun withNamedArgument

--- app/src/main/java/com/example/kotlin/Functions.kt
+++ app/src/main/java/com/example/kotlin/Functions.kt
@@ -11,3 +11,9 @@ fun withParameter(name: String){
     println("== withParameter ==")
     println("Hello, $name!")
 }
+
+fun withNamedArgument(name: String, age: Int){
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
