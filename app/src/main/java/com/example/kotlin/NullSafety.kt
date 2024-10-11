c9b86f96449f6fbcd215d03bb1bf8d2da830550c
mohnurwahid <mohnurwahid923@gmail.com>
2024-10-12 02:30:39 +0700

    membuat fun nullSafety

---
+++ app/src/main/java/com/example/kotlin/NullSafety.kt
@@ -0,0 +1,33 @@
+package com.example.kotlin
+
+import org.jetbrains.annotations.NotNull
+
+fun nullSafety(){
+    var neverNull: String = "This can't be null"
+
+
+    var nullable: String? = "You can keep a null here"
+
+    nullable = null
+
+
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inerredNodNull is not null")
+    }
+
+    println(neverNull.length)
+    println(nullable?.length)
+
+
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull))
+
+}
+fun main(){
+    nullSafety()
+}
